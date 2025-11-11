c7fffde76bca1c5e81acbe72a9dee40879be8b13
Syed Imran Hasan <imran.h94@gmail.com>
2025-11-11 06:28:11 +0500

    moved to dynamic content

--- blog/views.py
+++ blog/views.py
@@ -1,66 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 from datetime import date
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Imran",
-        "date": date(2024, 11, 9),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio asperiores maiores aut harum dolorum omnis molestias, tempore iusto numquam nihil repellat quasi ea aperiam animi excepturi accusantium. Minus, illo harum.
-            
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio asperiores maiores aut harum dolorum omnis molestias, tempore iusto numquam nihil repellat quasi ea aperiam animi excepturi accusantium. Minus, illo harum.
-
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio asperiores maiores aut harum dolorum omnis molestias, tempore iusto numquam nihil repellat quasi ea aperiam animi excepturi accusantium. Minus, illo harum.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Imran",
-        "date": date(2024, 12, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Imran",
-        "date": date(2025, 11, 9),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
+all_posts = []
 
 def get_date(post):
     return post['date']
@@ -69,8 +12,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts 
     })
